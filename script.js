45c2b0e39e33f7c7de44a4670051217db799fd0e
Ahmeda Farhin <124598487+aFarhin@users.noreply.github.com>
2023-04-12 12:19:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+
+  function onLoad() {
+        const body = document.querySelector('body');
+        body.innerHTML = 'DOM load success';
+      }
+
+window.addEventListener('DOMContentLoaded', onLoad);
